cc626c552fd2f2b15266ad33af9a6d98b2dee124
Md. Ibrahim khalil <mdibrahimkhalil505@gmail.com>
2022-02-16 21:38:34 +0600

    this is our ebangla account

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
 document.getElementById('login-btn').addEventListener('click', function(){
-    console.log('button clicked');
+    const userIdFiled = document.getElementById('user-id');
+    const userId = userIdFiled.value;
+    
+    const userMobileFiled = document.getElementById('user-mobile');
+    const userMobile = userMobileFiled.value;
+
+    if(userId == 'ebangla' && userMobile == 'ebangla2020'){
+        window.location.href = 'main.html';
+    }else{
+        alert('ID or Phone Number Is Wrong')
+    }
+
+    
+
+
 })
